ff5ba7294b613e7265b74ecb1d3078faa7c8d322
Quincy Stafford <xXQS316Xx@gmail.com>
2021-09-21 13:13:19 -0500

    added button styles

--- 99lines.js
+++ 99lines.js
@@ -5,7 +5,7 @@ let button = document.createElement("button")
 let btnText = document.createTextNode("Sing!")
 
 button.appendChild(btnText)
-
+button.className = 'singbtn'
 document.body.appendChild(button)
 
 })
