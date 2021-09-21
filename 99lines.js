536dd566e5822fa7b4e1e59b1b53db5954b4de28
Quincy Stafford <xXQS316Xx@gmail.com>
2021-09-21 13:28:47 -0500

    added 99 lines of code

--- 99lines.js
+++ 99lines.js
@@ -8,8 +8,27 @@ document.addEventListener("DOMContentLoaded", function () {
     button.className = 'singbtn'
     document.body.appendChild(button)
 
-    button.addEventListener("click", function(){
-        
+    button.addEventListener("click", function () {
+        for (let i = 0; i < friends.length; i++) {
+            for (let j = 99; j > 0; j--) {
+                if (j > 2) {
+                    console.log(j, ' Lines of code in the file, ' + j + ' lines of code;'
+                        + friends[i] + ' strikes one out clears it all out, ' + (j - 1) +
+                        ' lines of code in the file')
+                }
+                else if (j == 2) {
+                    console.log(j + ' Lines of code in the file, ' + j + ' lines of code; '
+                        + friends[i] + ' strikes one out clears it all out, ' + (j - 1) +
+                        ' line of code in the file')
+                }
+                else {
+                    console.log(j + ' Line of code in the file, ' + j + ' line of code; '
+                        + friends[i] + ' strikes one out clears it all out, ' + (j - 1) +
+                        ' line of code in the file ')
+                }
+
+            }
+        }
     })
 
 })
