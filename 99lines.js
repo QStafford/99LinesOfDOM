21fb8dfdd60d9631796c99ee8f3d5cc02d067f0e
Quincy Stafford <xXQS316Xx@gmail.com>
2021-09-21 13:10:15 -0500

    commences after page is loaded

--- 99lines.js
+++ 99lines.js
@@ -1,6 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+
 let button = document.createElement("button")
 let btnText = document.createTextNode("Sing!")
 
 button.appendChild(btnText)
 
 document.body.appendChild(button)
+
+})
