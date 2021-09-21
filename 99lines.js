b0757137e354c5ebfb41345daf73e66418d8c66a
Quincy Stafford <xXQS316Xx@gmail.com>
2021-09-21 13:20:16 -0500

    added click function

--- 99lines.js
+++ 99lines.js
@@ -1,11 +1,15 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
+    let friends = ['Jerry', 'Kevin', 'Diego', 'Alex', 'Emma']
+    let button = document.createElement("button")
+    let btnText = document.createTextNode("Sing!")
 
-let button = document.createElement("button")
-let btnText = document.createTextNode("Sing!")
+    button.appendChild(btnText)
+    button.className = 'singbtn'
+    document.body.appendChild(button)
 
-button.appendChild(btnText)
-button.className = 'singbtn'
-document.body.appendChild(button)
+    button.addEventListener("click", function(){
+
+    })
 
 })
